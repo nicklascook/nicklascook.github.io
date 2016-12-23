48d0bc9f6905c8393c7609ed90274fc63bfc3f33
Nick <nicklascook@hotmail.com>
2016-12-23 17:01:23 +1000

    inital commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () { // doc ready
+
+    // portfolio images animation
+    $('.std-mobile').animate({left:"0px", bottom:"0px", opacity:"1"}, 2500);
+    $('.std-desktop').animate({left:"0px", top:"0px", opacity:"1"}, 2000);
+    $('.rtm-mobile').animate({left:"0px", bottom:"0px", opacity:"1"}, 2500);
+    $('.rtm-desktop').animate({left:"0px", top:"0px", opacity:"1"}, 2000);
+    //------
+
+});// END
